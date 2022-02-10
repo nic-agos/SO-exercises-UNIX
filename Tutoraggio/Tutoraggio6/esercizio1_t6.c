707a8382762275ce76a6d7fe19b91a131a02e570
nicco <niccolo.agostinelli@gmail.com>
2022-02-10 09:44:05 +0100

    added text exercise1, tutor6

---
+++ Tutoraggio/Tutoraggio6/esercizio1_t6.c
@@ -0,0 +1,14 @@
+/*
+Si supponga di avere un file system che supporta il
+metodo di allocazione indicizzato, in cui il record di
+sistema associato ad ogni file mantenga 120 indici diretti,
+4 indici indiretti e 4 indici doppiamente indiretti. Si
+supponga inoltre che il dispositivo di memoria di massa
+ove il file system è ospitato abbia blocchi di taglia pari a
+1024 record, e che un indice di blocco di dispositivo sia
+esperesso con 8 record. Si indichi la massima taglia
+possibile (in termini di numero di record) per un generico
+file allocato su dispositivo secondo tale schema di
+indicizzazione.
+*/
+
